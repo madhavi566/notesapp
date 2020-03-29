caec57f9631227495f97e58ffde3dfebd9d25e88
madhavi566 <madhavibogani@gmail.com>
2020-03-29 19:24:36 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import {Component, OnInit } from '@angular/core';    
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'notesapp';
-}
+export class MyItems {    
+  Value: string;    
+  constructor(Value:string)    
+  {    
+    this.Value = Value;    
+  }    
+}    
+    
+@Component({    
+  selector: 'app-root',    
+  templateUrl: './app.component.html',    
+  styleUrls: ['./app.component.css']    
+})    
+export class AppComponent  implements OnInit{    
+
+  title = 'Working With Array In Angular 5';    
+  today: number = Date.now();
+  note1: number = Date.now();
+
+
+  ngOnInit() {
+ }
+  // Array where we are going to do CRUD operations    
+  myItems: MyItems[] = new Array();    
+    
+  // Other variables    
+  IsForUpdate: boolean = false;    
+  newItem: any = {};    
+  updatedItem;    
+    
+  // Provide some values to the array    
+  constructor()    
+  {    
+    this.myItems.push(    
+      new MyItems("this is to update schedule"),    
+      new MyItems("internal meeting at 2pm"),    
+      new MyItems("conference call with clients"),    
+    
+    );   
+  }     
+
+  lastModified: Date;
+
+ AddItem() {    
+  this.myItems.push(    
+      this.newItem    
+  );    
+    this.newItem = {};    
+}    
+
+// When user selects edit option  
+
+
+EditItem(i) {  
+  this.newItem.Value = this.myItems[i].Value;  
+  this.updatedItem = i;  
+  this.IsForUpdate = true;  
+}  
+  
+// When user clicks on update button to submit updated value  
+UpdateItem() {  
+  let data = this.updatedItem;  
+  for (let i = 0; i < this.myItems.length; i++) {  
+    if (i == data) {  
+      this.myItems[i].Value = this.newItem.Value;  
+    }  
+  }  
+  this.IsForUpdate = false;  
+  this.newItem = {};  
+}  
+
+// To delete specific item  
+DeleteItem(i) {  
+  this.myItems.splice(i, 1);  
+}  
+
+}    
